89bd47420cfcd6afba89f62a751fd5dae141165f
soonkuk <soonkuk@gmail.com>
2023-04-17 17:33:55 +0900

    feat: add redeem_tokens operation

---
+++ sto/redeem_tokens.go
@@ -0,0 +1,126 @@
+package sto
+
+import (
+	"github.com/ProtoconNet/mitum-currency/v2/currency"
+	"github.com/ProtoconNet/mitum2/base"
+	"github.com/ProtoconNet/mitum2/util"
+	"github.com/ProtoconNet/mitum2/util/hint"
+	"github.com/ProtoconNet/mitum2/util/valuehash"
+)
+
+var (
+	RedeemTokensFactHint = hint.MustNewHint("mitum-sto-redeem-tokens-operation-fact-v0.0.1")
+	RedeemTokensHint     = hint.MustNewHint("mitum-sto-redeem-tokens-operation-v0.0.1")
+)
+
+type RedeemTokensItem interface {
+	hint.Hinter
+	util.IsValider
+	Bytes() []byte
+	Amount() currency.Amount
+	Account() base.Address
+	Rebuild() RedeemTokensItem
+}
+
+type RedeemTokensFact struct {
+	base.BaseFact
+	sender base.Address
+	items  []RedeemTokensItem
+}
+
+func NewRedeemTokensFact(token []byte, receiver base.Address, items []RedeemTokensItem) RedeemTokensFact {
+	bf := base.NewBaseFact(RedeemTokensFactHint, token)
+	fact := RedeemTokensFact{
+		BaseFact: bf,
+		sender:   receiver,
+		items:    items,
+	}
+	fact.SetHash(fact.GenerateHash())
+
+	return fact
+}
+
+func (fact RedeemTokensFact) Hash() util.Hash {
+	return fact.BaseFact.Hash()
+}
+
+func (fact RedeemTokensFact) GenerateHash() util.Hash {
+	return valuehash.NewSHA256(fact.Bytes())
+}
+
+func (fact RedeemTokensFact) Bytes() []byte {
+	is := make([][]byte, len(fact.items))
+	for i := range fact.items {
+		is[i] = fact.items[i].Bytes()
+	}
+
+	return util.ConcatBytesSlice(
+		fact.Token(),
+		fact.sender.Bytes(),
+		util.ConcatBytesSlice(is...),
+	)
+}
+
+func (fact RedeemTokensFact) IsValid(b []byte) error {
+	if err := fact.BaseHinter.IsValid(nil); err != nil {
+		return err
+	}
+
+	if n := len(fact.items); n < 1 {
+		return util.ErrInvalid.Errorf("empty items")
+	}
+
+	if err := util.CheckIsValiders(nil, false, fact.sender); err != nil {
+		return err
+	}
+
+	for i := range fact.items {
+		if err := util.CheckIsValiders(nil, false, fact.items[i]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (fact RedeemTokensFact) Token() base.Token {
+	return fact.BaseFact.Token()
+}
+
+func (fact RedeemTokensFact) Sender() base.Address {
+	return fact.sender
+}
+
+func (fact RedeemTokensFact) Items() []RedeemTokensItem {
+	return fact.items
+}
+
+func (fact RedeemTokensFact) Rebuild() RedeemTokensFact {
+	items := make([]RedeemTokensItem, len(fact.items))
+	for i := range fact.items {
+		it := fact.items[i]
+		items[i] = it.Rebuild()
+	}
+
+	fact.items = items
+	fact.SetHash(fact.GenerateHash())
+
+	return fact
+}
+
+type RedeemTokens struct {
+	currency.BaseOperation
+}
+
+func NewRedeemTokens(fact RedeemTokensFact) (RedeemTokens, error) {
+	return RedeemTokens{BaseOperation: currency.NewBaseOperation(MintSecurityTokensHint, fact)}, nil
+}
+
+func (op *RedeemTokens) HashSign(priv base.Privatekey, networkID base.NetworkID) error {
+	err := op.Sign(priv, networkID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
