c48c7e88b7b70d2343e5e481fa3aedef57980ad9
soonkuk <soonkuk@gmail.com>
2023-04-18 17:30:19 +0900

    feat: update create_security_tokens operation

--- sto/create_security_tokens_item.go
+++ sto/create_security_tokens_item.go
@@ -9,20 +9,20 @@ import (
 
 type BaseCreateSecurityTokensItem struct {
 	hint.BaseHinter
-	stoID             currencyextension.ContractID // token id
-	granularity       uint64                       // token granulariry
-	defaultPartitions []Partition                  // default partitions
-	controllers       []base.Address               // initial controllers
-	currency          currency.CurrencyID          // fee
+	stoID            currencyextension.ContractID // token id
+	granularity      uint64                       // token granulariry
+	defaultPartition Partition                    // default partitions
+	controllers      []base.Address               // initial controllers
+	currency         currency.CurrencyID          // fee
 }
 
-func NewBaseCreateSecurityTokensItem(ht hint.Hint, stoID currencyextension.ContractID, granularity uint64, partitions []Partition, controllers []base.Address, currency currency.CurrencyID) BaseCreateSecurityTokensItem {
+func NewBaseCreateSecurityTokensItem(ht hint.Hint, stoID currencyextension.ContractID, granularity uint64, partition Partition, controllers []base.Address, currency currency.CurrencyID) BaseCreateSecurityTokensItem {
 	return BaseCreateSecurityTokensItem{
-		BaseHinter:        hint.NewBaseHinter(ht),
-		stoID:             stoID,
-		granularity:       granularity,
-		defaultPartitions: partitions,
-		controllers:       controllers,
-		currency:          currency,
+		BaseHinter:       hint.NewBaseHinter(ht),
+		stoID:            stoID,
+		granularity:      granularity,
+		defaultPartition: partition,
+		controllers:      controllers,
+		currency:         currency,
 	}
 }
